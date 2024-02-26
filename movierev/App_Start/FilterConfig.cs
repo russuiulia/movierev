a0374a7c46a9921c9433fb8bbab572e0fa790960
russuiulia <russuiulia.md@gmail.com>
2024-02-26 02:20:26 +0200

    Add project files.

---
+++ movierev/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace movierev
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
